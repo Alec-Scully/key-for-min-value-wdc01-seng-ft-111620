628bcb2f7c413733a1ba8f15eb941bab1d67e4a7
Alec Scully <scullyalec@gmail.com>
2020-10-05 22:59:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,10 @@ def key_for_min_value(name_hash)
   test  = name_hash.values
   min = test[0]
   name_hash.each do |key, value|
-    if value < min 
-      min = value
+    temp_min = value
+    if temp_min <= min 
+      min = temp_min
       return key
-      binding.pry
     end 
   end 
 end
