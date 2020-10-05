12614623d0b4b1ab18e46c3dabdfafebabee60a8
Alec Scully <scullyalec@gmail.com>
2020-10-05 21:58:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash
+  name_hash.each do |key, value|
+    min = value 
+    temp_min = value
+    if temp_min < min 
+      min = temp_min
+      return key
+    end 
+  end 
 end
+
